d7235a1b5ec8d03177781cb65dcb1e0e2ee11c80
Daniel Goldstein <danielgold95@gmail.com>
2023-12-15 23:20:28 -0500

    add more explanation

--- Foo.scala
+++ Foo.scala
@@ -1,5 +1,8 @@
 package is.hail;
 
+// Without this import, `scalac` will compile Foo$.class to load `scala.Predef$`,
+// which creates the `Map` type alias. Something about `scala.Predef$` explodes
+// in CheerpJ.
 // If you uncomment this line, then `scalac`
 // does not appear to insert a load of scala.Predef
 // into the bytecode and running Foo.main in CJ succeeds

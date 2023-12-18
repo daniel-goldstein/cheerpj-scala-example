ded8f802ee5db0ccd1724ba0fc974a9deb876caa
Daniel Goldstein <danielgold95@gmail.com>
2023-12-18 15:37:54 -0500

    update for initializer error

--- Foo.scala
+++ Foo.scala
@@ -1,16 +1,14 @@
 package is.hail;
 
-// Without this import, `scalac` will compile Foo$.class to load `scala.Predef$`,
-// which creates the `Map` type alias. Something about `scala.Predef$` explodes
-// in CheerpJ.
-// If you uncomment this line, then `scalac`
-// does not appear to insert a load of scala.Predef
-// into the bytecode and running Foo.main in CJ succeeds
-// import scala.collection.immutable.Map;
-
 object Foo {
-    val bar: Map[String, String] = Map.empty
+    val defaults = Map[String, String](
+      ("a" -> "a"),
+      ("b" -> "b"),
+      ("c" -> "c"),
+      ("d" -> "d"),
+      ("e" -> "e"),
+    )
     def main(argv: Array[String]): Unit = {
-        System.out.println(argv(0))
+        System.out.println(defaults)
     }
 }
--- Makefile
+++ Makefile
@@ -2,19 +2,23 @@
 
 SCALA_VERSION ?= 2.12.18
 
+EMPTY :=
+SPACE := $(EMPTY) $(EMPTY)
+
 JARS := cheerpj-scala-example/scala-library.jar cheerpj-scala-example/hail.jar
+JCP := $(subst $(SPACE),:,$(JARS))
 
 cheerpj-scala-example/scala-library.jar:
 	curl -o $@ https://repo1.maven.org/maven2/org/scala-lang/scala-library/$(SCALA_VERSION)/scala-library-$(SCALA_VERSION).jar
 
 is/hail/%.class: Foo.scala
-	scalac Foo.scala
+	scalac *.scala
 
 cheerpj-scala-example/hail.jar: is/hail/Foo.class is/hail/Foo$$.class
 	jar cvf $@ $^
 
 run-java: $(JARS)
-	java -cp hail.jar:scala-library.jar is.hail.Foo "Hello world!"
+	java -cp $(JCP) is.hail.Foo "Hello world!"
 
 run-cheerpj: $(JARS)
 	npx vite
